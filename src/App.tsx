0ea6ec201016da0861d06b35220c57a9843e3909
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2024-05-11 21:12:43 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,68 @@
-import { PrivateRoute, routes } from '@/routes';
-import { Route, Routes } from 'react-router-dom';
-import { Layout, ToastContainer } from './components';
-import { generateKeywords } from './utils';
+import { PrivateRoute, routes } from "@/routes";
+import { Route, Routes } from "react-router-dom";
+import { Layout, ToastContainer } from "./components";
+import { generateKeywords } from "./utils";
 
 const App = () => {
-   generateKeywords('asfasdf');
-   generateKeywords('Vo Hoang Huy');
+  generateKeywords("asfasdf");
+  generateKeywords("Vo Hoang Huy");
 
-   return (
-      <div>
-         <ToastContainer />
-         <Routes>
-            {routes.map((route) => {
-               if (route.isPrivate && route.layout) {
-                  return (
-                     <Route
-                        path={route.link}
-                        element={
-                           <PrivateRoute>
-                              <Layout>
-                                 <route.component />
-                              </Layout>
-                           </PrivateRoute>
-                        }
-                        key={route.id}
-                     />
-                  );
-               } else if (route.isPrivate && !route.layout) {
-                  return (
-                     <Route
-                        path={route.link}
-                        element={
-                           <PrivateRoute>
-                              <route.component />
-                           </PrivateRoute>
-                        }
-                        key={route.id}
-                     />
-                  );
-               } else if (!route.isPrivate && route.layout) {
-                  return (
-                     <Route
-                        path={route.link}
-                        element={
-                           <Layout>
-                              <route.component />
-                           </Layout>
-                        }
-                        key={route.id}
-                     />
-                  );
-               }
+  return (
+    <div>
+      <ToastContainer />
+      <Routes>
+        {routes.map((route) => {
+          if (route.isPrivate && route.layout) {
+            return (
+              <Route
+                path={route.link}
+                element={
+                  <PrivateRoute>
+                    <Layout>
+                      <route.component />
+                    </Layout>
+                  </PrivateRoute>
+                }
+                key={route.id}
+              />
+            );
+          } else if (route.isPrivate && !route.layout) {
+            return (
+              <Route
+                path={route.link}
+                element={
+                  <PrivateRoute>
+                    <route.component />
+                  </PrivateRoute>
+                }
+                key={route.id}
+              />
+            );
+          } else if (!route.isPrivate && route.layout) {
+            return (
+              <Route
+                path={route.link}
+                element={
+                  <Layout>
+                    <route.component />
+                  </Layout>
+                }
+                key={route.id}
+              />
+            );
+          }
 
-               return (
-                  <Route
-                     key={route.id}
-                     path={route.link}
-                     element={<route.component />}
-                  />
-               );
-            })}
-         </Routes>
-      </div>
-   );
+          return (
+            <Route
+              key={route.id}
+              path={route.link}
+              element={<route.component />}
+            />
+          );
+        })}
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,26 @@
-import '@/styles/index.scss';
-import ReactDOM from 'react-dom/client';
-import 'react-loading-skeleton/dist/skeleton.css';
-import { BrowserRouter } from 'react-router-dom';
-import 'tippy.js/dist/tippy.css';
-import App from './App';
+import "@/styles/index.scss";
+import ReactDOM from "react-dom/client";
+import "react-loading-skeleton/dist/skeleton.css";
+import { BrowserRouter } from "react-router-dom";
+import "tippy.js/dist/tippy.css";
+import App from "./App";
 import {
-   AuthProvider,
-   ChatProvider,
-   TabProvider,
-   ToastProvider,
-} from './contexts';
+  AuthProvider,
+  ChatProvider,
+  TabProvider,
+  ToastProvider,
+} from "./contexts";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-   <BrowserRouter>
-      <AuthProvider>
-         <ToastProvider>
-            <ChatProvider>
-               <TabProvider>
-                  <App />
-               </TabProvider>
-            </ChatProvider>
-         </ToastProvider>
-      </AuthProvider>
-   </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <ToastProvider>
+        <ChatProvider>
+          <TabProvider>
+            <App />
+          </TabProvider>
+        </ChatProvider>
+      </ToastProvider>
+    </AuthProvider>
+  </BrowserRouter>
 );
